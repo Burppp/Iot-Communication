bcea1496f1abcc2d1add4e03f0bb74ed8f43a777
burppp <1483543906@qq.com>
2024-05-23 23:03:06 +0800

    esp32_now recv

--- finalWork/ESP_Now_Slave/ESP_Now_Slave.ino
+++ finalWork/ESP_Now_Slave/ESP_Now_Slave.ino
@@ -75,6 +75,7 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
             mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
     Serial.print("Last Packet Recv from: "); Serial.println(macStr);
     Serial.print("Last Packet Recv Data: "); Serial.println(*data);
+    Serial.print("Last Packet Recv Data len: ");Serial.println(data_len);
     Serial.println("");
 }
 
