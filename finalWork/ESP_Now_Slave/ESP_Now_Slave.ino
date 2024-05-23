fd8cfbcec8a887e569db11532a2d2387b5b90fa6
burppp <1483543906@qq.com>
2024-05-23 23:10:17 +0800

    esp32 usart sender

--- finalWork/ESP_Now_Slave/ESP_Now_Slave.ino
+++ finalWork/ESP_Now_Slave/ESP_Now_Slave.ino
@@ -22,11 +22,11 @@ void InitESPNow()
     WiFi.disconnect();
     if (esp_now_init() == ESP_OK) 
     {
-        Serial.println("ESPNow Init Success");
+        // Serial.println("ESPNow Init Success");
     }
     else 
     {
-        Serial.println("ESPNow Init Failed");
+        // Serial.println("ESPNow Init Failed");
         // Retry InitESPNow, add a counte and then restart?
         // InitESPNow();
         // or Simply Restart
@@ -41,25 +41,25 @@ void configDeviceAP()
     bool result = WiFi.softAP(SSID, "Slave_1_Password", CHANNEL, 0);
     if (!result) 
     {
-        Serial.println("AP Config failed.");
+        // Serial.println("AP Config failed.");
     } 
     else 
     {
-        Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
-        Serial.print("AP CHANNEL "); Serial.println(WiFi.channel());
+        // Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+        // Serial.print("AP CHANNEL "); Serial.println(WiFi.channel());
     }
 }
 
 void setup() 
 {
     Serial.begin(115200);
-    Serial.println("ESPNow Slave");
+    // Serial.println("ESPNow Slave");
     //Set device in AP mode to begin with
     WiFi.mode(WIFI_AP);
     // configure device AP mode
     configDeviceAP();
     // This is the mac address of the Slave in AP Mode
-    Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+    // Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
     // Init ESPNow with a fallback logic
     InitESPNow();
     // Once ESPNow is successfully Init, we will register for recv CB to
@@ -73,10 +73,12 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
     char macStr[18];
     snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
             mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-    Serial.print("Last Packet Recv from: "); Serial.println(macStr);
-    Serial.print("Last Packet Recv Data: "); Serial.println(*data);
-    Serial.print("Last Packet Recv Data len: ");Serial.println(data_len);
-    Serial.println("");
+    // Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+    // Serial.print("Last Packet Recv Data: "); 
+    Serial.println(data_len);
+    Serial.println(*data);
+    // Serial.print("Last Packet Recv Data len: ");
+    // Serial.println("");
 }
 
 uint8_t RGB_val[3] = {0x00, 0x00, 0x00};
